b6b97029c61478e888979aed593edf655f667fd7
hschuler13 <hschul04@gmail.com>
2024-01-30 22:36:16 -0600

    HW1.1

---
+++ HW1.2/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(700, 400);
+}
+
+function draw() {
+  background(220);
+  fill('red');
+  square(0,0,15);
+  fill('orange');
+  square(0,15,15);
+  fill('yellow');
+  square(0,30,15);
+  fill('green');
+  square(0,45,15);
+  fill('cyan');
+  square(0,60,15);
+  fill('blue');
+  square(0,75,15);
+  fill('magenta');
+  square(0,90,15);
+  fill('brown');
+  square(0,105,15);
+  fill('white');
+  square(0,120,15);
+  fill('black');
+  square(0,135,15);
+}
+
+function mouseClicked(){
+  
+}
