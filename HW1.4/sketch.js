f603bc3b7d3884998aac1701037233151d75c885
hschuler13 <hschul04@gmail.com>
2024-02-20 13:55:45 -0600

    HW1.4

--- HW1.4/sketch.js
+++ HW1.4/sketch.js
@@ -1,46 +1,67 @@
 let sprite;
 let beetle;
+let font;
+let bugs = [];
+let screen = 1;
+let timerLength = 30;
+let deadBugs = 0;
+
 function preload(){
-
-}
-
-function setup() {
-  createCanvas(400, 400);
-  /*let color = 'green';
-  let gameEndBoo;
-  let score;
-  let time;*/
-
   let animations = {
     walk: {row: 0, frames: 7},
     squish: {row: 1, frames: 1}
   }
+
+  //use Math.floor(Math.random() * num);
   beetle = new Character (200,200,32,32,'assets/beetle(1).png',animations);
-  //gameStart();
+}
+
+function setup() {
+  createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
-  
-  //gameStart();
+  //beetle.left();
+  if (screen == 1){
+    gameStart();
+  }
+  else if (screen == 2){
+    gameEnd();
+  }
+  else{
+    gamePlaying();
+  }
 
 }
 
-/*function gameStart(){
+function gameStart(){
   text("Press the space button to start!", 200, 200);
+  if (kb.pressing(' ')){
+    screen = 0;
+  }
 }
 
 function gamePlaying(){
-
+  text("timer: " + ceil(timerLength),300,20);
+  text("score: " + deadBugs,100,20);
+  timerLength -= deltaTime/1000;
+  if (timerLength < 0){
+    screen = 2;
+  }
 }
 
 function gameEnd(){
   text("Game end!", 200, 150);
-  text("Score: ", 200, 300);
+  text("Bugs squashed: ", 200, 300);
   text("Press the space button to restart!", 200, 200);
+  if (kb.pressing(' ')){
+    screen = 0;
+    timerLength = 30;
+  }
 }
 
-function keyTyped(){
+/*function keyTyped(){
   if (key === ''){
     color = 'green';
   }
@@ -54,5 +75,29 @@ class Character{
     this.sprite.anis.frameDelay = 7;
     this.sprite.addAnis(animations);
     this.sprite.changeAni('walk');
+  }
+
+  /*stop(){
+
+  }*/
+
+  left(){
+    this.sprite.changeAni('walk');
+    this.sprite.rotation(90)
+  }
+
+  right(){
+    this.sprite.changeAni('walk');
+    this.sprite.rotation(270);
+  }
+
+  up(){
+    this.sprite.changeAni('walk');
+    this.sprite.rotation(0);
+  }
+
+  down(){
+    this.sprite.changeAni('walk');
+    this.sprite.rotation(180);
   }
 }
