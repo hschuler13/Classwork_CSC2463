2c87b15b919c09b6dd498acb4e027567b0b413ba
hschuler13 <hschul04@gmail.com>
2024-03-19 17:56:28 -0500

    HW 2.3

--- HW2.3/sketch.js
+++ HW2.3/sketch.js
@@ -1,7 +1,19 @@
 let image;
 
+//let synth = new Tone.PolySynth(Tone.DuoSynth);
+//let bitC = new Tone.BitCrusher(4);
+
+//synth.connect(bitC);
+//bitC.toDestination();
+//synth.toDestination();
+
+//D4
+//C#4
+//C4
+//B3
+
 function preload() {
-  image = loadImage('StarMario.png')
+  image = loadImage('StarMario.png');
 }
 
 function setup() {
@@ -12,10 +24,17 @@ function setup() {
 function draw() {
   if (mouseIsPressed === true){
     background(image);
-    
   }
   else{
     background(220);
     text ('Press mouse for a surprise!', 200, 200);
   }
 }
+
+/*function mousePressed() {
+  synth.triggerAttack('D4');
+  synth.triggerAttack('C#4');
+  synth.triggerAttack('C4');
+  synth.triggerAttack('B3');
+  Tone.Transport.start();
+}*/
---
+++ HW2.4/sketch.js
@@ -0,0 +1,67 @@
+let palettes;
+let chosenPalette;
+
+function setup() {
+  createCanvas(700, 400);
+  chosenPalette = color('white');
+  palettes = [
+    new Palette(0,0,color('red')),
+    new Palette(0,15,color('orange')),
+    new Palette(0,30,color('yellow')),
+    new Palette(0,45,color('green')),
+    new Palette(0,60,color('cyan')),
+    new Palette(0,75,color('blue')),
+    new Palette(0,90,color('magenta')),
+    new Palette(0,105,color('brown')),
+    new Palette(0,120,color('white')),
+    new Palette(0,135,color('black')),
+  ];
+}
+
+function draw() {
+  stroke('black');
+  noFill();
+  strokeWeight(2);
+  rect(0,0,700,400);
+
+  for(let i=0;i < palettes.length;i++) {
+    palettes[i].draw();
+  }
+
+  if(mouseIsPressed){
+    stroke(chosenPalette);
+    strokeWeight(5);
+    line (mouseX, mouseY, pmouseX, pmouseY);
+  } 
+}
+
+function mousePressed(){
+  let inPalette = false;
+  for(let i=0; i < palettes.length; i++){
+    if(palettes[i].contains(mouseX,mouseY)){
+      chosenPalette = palettes[i].fill;
+      inPalette = true;
+    }
+  }
+}
+
+class Palette{
+  constructor (x, y, fill){
+    this.x = x;
+    this.y = y;
+    this.fill = fill;
+  }
+
+  draw(){
+    fill(this.fill);
+    strokeWeight(1);
+    stroke('black');
+    square(this.x,this.y,15);
+  }
+
+  contains(x,y){
+    let inX = x >= this.x && x <= this.x + 100;
+    let inY = y >= this.y && y <= this.y +100;
+    return inX && inY;
+  }
+}
