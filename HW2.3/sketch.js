c2bcac660437a4e8e4f2359d8340fa67406c5c4d
hschuler13 <hschul04@gmail.com>
2024-03-19 19:54:27 -0500

    HW 2.3

--- HW2.3/sketch.js
+++ HW2.3/sketch.js
@@ -1,24 +1,14 @@
 let image;
 
-//let synth = new Tone.PolySynth(Tone.DuoSynth);
-//let bitC = new Tone.BitCrusher(4);
-
-//synth.connect(bitC);
-//bitC.toDestination();
-//synth.toDestination();
-
-//D4
-//C#4
-//C4
-//B3
+let bc = new Tone.BitCrusher(8).toDestination();
+let synth = new Tone.PolySynth().connect(bc);
 
 function preload() {
-  image = loadImage('StarMario.png');
+  image = loadImage('sadTrombone.jpg');
 }
 
 function setup() {
-  createCanvas(400, 400);
-  textAlign(CENTER);
+  createCanvas(400,400);
 }
 
 function draw() {
@@ -27,14 +17,19 @@ function draw() {
   }
   else{
     background(220);
-    text ('Press mouse for a surprise!', 200, 200);
   }
 }
 
-/*function mousePressed() {
-  synth.triggerAttack('D4');
-  synth.triggerAttack('C#4');
-  synth.triggerAttack('C4');
-  synth.triggerAttack('B3');
+function mousePressed() {
+  synth.sync();
+  synth.triggerAttackRelease("D4", "8n", 0);
+  synth.triggerAttackRelease("C#4", "8n", +.5);
+  synth.triggerAttackRelease("C4", "8n", +1);
+  synth.triggerAttackRelease("B3", "2n", +1.5);
   Tone.Transport.start();
-}*/
+}
+
+function mouseReleased() {
+  Tone.Transport.stop();
+  
+}
