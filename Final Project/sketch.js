c1e0a48e2cff11720d50754c5031be1c74f2d163
hschuler13 <hschul04@gmail.com>
2024-04-29 21:25:12 -0500

    final project wip 1

---
+++ Final Project/sketch.js
@@ -0,0 +1,128 @@
+let playerX = 400;
+let playerY = 375;
+let playerWidth = 30; //set these when you have drawn your sprites
+let playerHeight = 70;
+
+let platformX = 200; //change these when you make your own custom platforms
+let platformY = 300;
+let platformWidth = 200;
+let platformHeight = 40;
+
+let gameStateLevel = 0;
+
+let gravityJump = false;
+let gravityPlayerDirection = 1;
+let gravityPlayerVelocity = 2;
+let gravityJumpHeight = 15;
+let gravityFallSpeed = 2;
+let gravityGroundHeight = 375;
+let gravitySkyHeight = 50;
+let gravityJumpCount = 0;
+
+function setup() {
+  createCanvas(800, 500);
+  rectMode(CENTER);
+  textAlign(CENTER);
+  //grass = new Sprite();
+}
+
+function draw() {
+  keyPressed();
+  keyTyped();
+  gravity();
+  if(gameStateLevel == 0){
+    playState();
+  }
+}
+
+function playState(){
+  //background
+  background(150,230,240);
+  
+  //grass
+  noStroke();
+  fill(100,200,75);
+  rect(width/2,450,width,100);
+  /*grass.width = 800;
+  grass.height = 100;
+  grass.color = 'green';
+  grass.x = 400;
+  grass.y = 450;
+  push();*/
+
+  //border
+  noFill();
+  stroke(0);
+  strokeWeight(15);
+  rect(width/2,height/2,width,height);
+
+  //platform
+  stroke(0);
+  strokeWeight(5);
+  fill(255,120,0);
+  rect(platformX,platformY,platformWidth,platformHeight);
+
+  //character
+  stroke(0);
+  fill(150,0,170);
+  rect(playerX,playerY,playerWidth,playerHeight);
+
+  //collisions
+  if(playerX >= platformX - platformWidth/2 && playerX <= platformX + platformWidth/2 && playerY + playerHeight >= platformY - platformHeight/2 && playerY + playerHeight >= platformY + platformHeight/2 && jump == false){
+    playerY = playerY;
+    gravityPlayerVelocity = 0;
+    jumpCounter = 0;
+  }
+}
+
+function keyPressed(){
+  if(kb.pressing('a')){
+    playerX -= 5;
+  }
+  if(kb.pressing('d')){
+    playerX += 5;
+  }
+  /*else if(keyDown('UP_ARROW')){
+    playerY = playerY + 5;
+  }
+  else if(keyDown('DOWN_ARROW')){
+    playerY = playerY - 5;
+  }*/
+}
+
+function keyTyped(){
+  if(kb.pressing('s')){
+    gravityJump = true;
+  }
+  else {
+    gravityJump = false;
+  }
+}
+
+function gravity(){
+  if(playerY >= gravityGroundHeight && gravityJump == false){
+    playerY = playerY;
+    gravityJumpCount = 0;
+  }
+  else{
+    playerY += (gravityPlayerDirection*gravityPlayerVelocity);
+  }
+  
+  if(gravityJump == true){
+    if(playerY <= gravitySkyHeight || gravityJumpCount >= gravityJumpHeight){
+      if(playerY >= gravityGroundHeight){
+        playerY = gravityGroundHeight
+      }
+      else{
+        gravityPlayerVelocity = gravityFallSpeed;
+      }
+    }
+    else{
+      gravityPlayerVelocity -= gravityJumpHeight;
+      gravityJumpCount++;
+    }  
+  }
+  else{
+    gravityPlayerVelocity = gravityFallSpeed;
+  }
+}
--- HW3.4/sketch.js
+++ HW3.4/sketch.js
@@ -1,8 +1,15 @@
 let palettes;
+let paletteColors;
+let colorLED = [0, 0, 0];
 let chosenPalette;
 let clear;
 let drawLine = false;
 let lineCount = 0;
+let circleX, circleY;
+let speed = 3;
+let port;
+let joyX = 0, JoyY = 0, sw = 0;
+let saveB = 0, clearB = 0, selectB = 0;
 
 let sounds = new Tone.Players({
   'paintStroke': "assets/paintStroke.mp3",
@@ -18,7 +25,7 @@ let paintingSong = ["C6", "G6", "A6", "G6"];
 let tenLineSong = ["C2", "G2", "A2", "G2"];
 
 const seq = new Tone.Sequence((time, note) => {
-	synth.triggerAttackRelease(note, 0.1, time);
+  synth.triggerAttackRelease(note, 0.1, time);
 }, defaultSong);
 
 const seq2 = new Tone.Sequence((time, note) => {
@@ -35,113 +42,183 @@ Tone.Transport.start();
 
 function setup() {
   createCanvas(700, 400);
-  
-  chosenPalette = color('white');
+  circleX = width / 2;
+  circleY = height / 2;
+  port = createSerial();
+  connectButton = createButton("Connect");
+  connectButton.mousePressed(connect);
+
+  chosenPalette = 0;
+
   palettes = [
-    new Palette(0,0,color('red')),
-    new Palette(0,15,color('orange')),
-    new Palette(0,30,color('yellow')),
-    new Palette(0,45,color('green')),
-    new Palette(0,60,color('cyan')),
-    new Palette(0,75,color('blue')),
-    new Palette(0,90,color('magenta')),
-    new Palette(0,105,color('brown')),
-    new Palette(0,120,color('white')),
-    new Palette(0,135,color('black')),
+    new Palette(0, 0, color('red')),
+    new Palette(0, 15, color('orange')),
+    new Palette(0, 30, color('yellow')),
+    new Palette(0, 45, color('green')),
+    new Palette(0, 60, color('cyan')),
+    new Palette(0, 75, color('blue')),
+    new Palette(0, 90, color('magenta')),
+    new Palette(0, 105, color('brown')),
+    new Palette(0, 120, color('white')),
+    new Palette(0, 135, color('black')),
   ];
 
+  paletteColors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'cyan',
+    'blue',
+    'magenta',
+    'brown',
+    'white',
+    'black'
+  ];
+
+  let usedPorts = usedSerialPorts();
+  if (usedPorts.length > 0) {
+    port.open(usedPorts[0], 9600);
+  }
+  frameRate(60);
   background(255);
 }
 
 function draw() {
-  seq.start();
-
   stroke('black');
   noFill();
   strokeWeight(2);
-  rect(0,0,700,400);
+  rect(0, 0, 700, 400);
 
-  for(let i=0;i < palettes.length;i++) {
+  for (let i = 0; i < palettes.length; i++) {
     palettes[i].draw();
   }
 
-  if(mouseIsPressed){
-    stroke(chosenPalette);
-    strokeWeight(5);
-    line (mouseX, mouseY, pmouseX, pmouseY);
-  } 
+  seq.start();
+  seq2.start();
+  seq3.start();
 
-  if(lineCount>10){
-    seq3.start();
+  let str = port.readUntil("\n");
+  let values = str.split(",");
+  if (values.length > 5) {
+    joyX = values[0];
+    joyY = values[1];
+    sw = Number(values[2]);
+    saveB = Number(values[3]);
+    clearB = Number(values[4]);
+    selectB = Number(values[5]);
+    //text(values, 100, 100);
+
+    if (joyX > 0) {
+      circleX += speed;
+    } else if (joyX < 0) {
+      circleX -= speed;
+    }
+
+    if (joyY > 0) {
+      circleY += speed;
+    } else if (joyY < 0) {
+      circleY -= speed;
+    }
+
   }
-}
 
-function keyPressed(){
-  if (keyCode === BACKSPACE){
-    sounds.player("paperCrumple").start();
-    background(255);
-    seq3.stop();
-    lineCount = 0;
-  }
+  noStroke();
+  fill(paletteColors[chosenPalette]);
+  circle(circleX, circleY, 10);
 
-  else if (keyCode === ENTER){
+  if (saveB == 1) {
     sounds.player("cameraClick").start();
     saveCanvas();
   }
-}
-
-function mousePressed(){
-  let inPalette = false;
-  lineCount++;
-    soundPaint();
-    seq2.start();
-
-  for(let i=0; i < palettes.length; i++){
-    if(palettes[i].contains(mouseX,mouseY) && mouseX<15 && mouseY<150){
-      inPalette = true;
-      chosenPalette = palettes[i].fill;
+  if (clearB == 1) {
+    sounds.player("paperCrumple").start();
+    circleX = width / 2;
+    circleY = height / 2;
+    background(255);
+    lineCount = 0;
+  }
+  if (selectB == 1) {
+    chosenPalette++;
+    if (chosenPalette == 10) {
+      chosenPalette = 0;
     }
   }
 
-  if(inPalette){
-    soundColSel();
+  if (port.opened()) {
+    let message = `${255} ${0} ${0}\n`;
+    switch (chosenPalette) {
+      case 0:
+        message = `${255} ${0} ${0}\n`;
+        break;
+      case 1:
+        message = `${255} ${100} ${0}\n`;
+        break;
+      case 2:
+        message = `${255} ${255} ${0}\n`;
+        break;
+      case 3:
+        message = `${0} ${255} ${0}\n`;
+        break;
+      case 4:
+        message = `${0} ${255} ${255}\n`;
+        break;
+      case 5:
+        message = `${0} ${0} ${255}\n`;
+        break;
+      case 6:
+        message = `${128} ${0} ${128}\n`;
+        break;
+      case 7:
+        message = `${139} ${69} ${19}\n`;
+        break;
+      case 8:
+        message = `${255} ${255} ${255}\n`;
+        break;
+      case 9:
+        message = `${0} ${0} ${0}\n`;
+        break;
+    }
+    port.write(message);
   }
-
 }
 
-function mouseReleased(){
-  sounds.player("paintStroke").stop();
-  sounds.player("paintSplash").stop();
-  seq2.stop();
-}
-
-function soundPaint(){
-  sounds.player("paintStroke").start();
+function soundPaint() {
   sounds.player("paintStroke").loop = true;
+  sounds.player("paintStroke").start();
+  
 }
 
-function soundColSel(){
+function soundColSel() {
   sounds.player("paintSplash").start();
   sounds.player("paintSplash").loop = false;
 }
 
-class Palette{
-  constructor (x, y, fill){
+class Palette {
+  constructor(x, y, fill) {
     this.x = x;
     this.y = y;
     this.fill = fill;
   }
 
-  draw(){
+  draw() {
     fill(this.fill);
     strokeWeight(1);
     stroke('black');
-    square(this.x,this.y,15);
+    square(this.x, this.y, 15);
   }
 
-  contains(x,y){
+  contains(x, y) {
     let inX = x >= this.x && x <= this.x + 100;
-    let inY = y >= this.y && y <= this.y +100;
+    let inY = y >= this.y && y <= this.y + 100;
     return inX && inY;
+  }
+}
+
+function connect() {
+  if (!port.opened()) {
+    port.open('Arduino', 9600);
+  } else {
+    port.close();
   }
 }
