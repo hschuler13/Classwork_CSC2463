ea59c9f27e70b1c2e618ee6bfe280191a25582ac
hschuler13 <hschul04@gmail.com>
2024-05-02 14:36:44 -0500

    Update sketch.js

--- FinalProj2/sketch.js
+++ FinalProj2/sketch.js
@@ -2,6 +2,7 @@ let player, onGround, tileMap;
 let gameState = 0;
 let playerPic, bgPic, topGroundPic, bottomGroundPic, carrotPic, iceBulletPic, fireBulletPic, fireEnemyPic, iceEnemyPic, fireObstaclePic, iceObstaclePic;
 let groundSize = 20;
+let jump = 20;
 
 function preload(){
   player = new Sprite(30,30,400,250);
@@ -70,7 +71,69 @@ function setup() {
 }
 
 function draw() {
-  background(135, 206, 235);
+  if(gameState == 1){
+    world.step();
+    background(135, 206, 235);
+    camera.x = player.x + 52;
+    camera.y = player.y;
+    player.visible = true;
+    walkable.visible = true;
+    playerMove();
+  }
+  /*else if(gameState == 2){
+
+  }*/
+  else {
+    background(220);
+    text("Press space to start", 400, 250);
+    player.visible = false;
+    walkable.visible = false;
+    if(kb.presses('space')){
+      gameState = 1;
+    }
+  }
+  
+}
+
+function playerMove(){
+  if(kb.pressing('a')){
+    player.vel.x = -2;
+    player.ani = 'run';
+    player.mirror.x = true;
+  }
+  else if(kb.pressing('d')){
+    player.vel.x = 2;
+    player.ani = 'run';
+    player.mirror.x = false;
+  }
+  else{
+    player.ani = 'stand';
+    player.vel.x = 0;
+  }
+
+  if(kb.presses('space') && onGround.overlapping(walkable)){
+    player.vel.y = jump;
+  }
+
+  //adjust for your specific tiles
+  if(onGround.overlapping(grass) || onGround.overlapping(water)){
+    player.drag = 20;
+    player.friction = 10;
+    jump = 15;
+    player.h = 8;
+  }
+  else{
+    player.drag = 0;
+    player.friction = 0;
+    jump = 20;
+    //player.h = 
+  }
+
+  if(player.y > 400){
+    player.speed = 0;
+    player.x = 40;
+    player.y = 100;
+  }
 }
 
 /*function keyPressed(){
